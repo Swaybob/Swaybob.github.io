e3466ad5f76d417a632d043eaf1305006e6624eb
Marvellous <adeoyemarvellous7@gmail.com>
2022-06-23 13:42:59 +0100

    Calculator error debug

--- Jscalculator/JScalculator.js
+++ Jscalculator/JScalculator.js
@@ -2,20 +2,18 @@ let firstNum = prompt('Enter your first number');
 let sign = prompt('which operation do you want to perform? + or - or * or /:')
 let secondNum = prompt('Enter the second number')
 
-// alert(firstNum)
-// alert(sign)
-// alert(secondNum)
+
 
 function Calculate() {
     if (sign == '+') {
-        alert(firstNum + secondNum);
+        alert(Number(firstNum) + Number(secondNum));
     } else if (sign == '-') {
-        alert(firstNum - secondNum);
+        alert(Number(firstNum) - Number(secondNum));
     } else if (sign == '*') {
-        alert(firstNum * secondNum);
+        alert(Number(firstNum) * Number(secondNum));
     }
     else {
-        alert(firstNum / secondNum);
+        alert(Number(firstNum) / Number(secondNum));
     }
 }
 Calculate()
